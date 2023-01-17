2cec827ef632eb9258dc827785e926125c08abec
PetarGanchev <lucifer@abv.bg>
2023-01-18 00:05:19 +0200

    add fire

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+var products  = document.getElementsByClassName('hot');
+for (let i=0; products.length>i; i++) {
+  let hot = document.getElementsByClassName('hot');
+   hot[i].innerHTML = hot[i].innerHTML+"🔥";
+
+}
+
 });
