57840b9fa291aef91617aa8dbc783bbf94d8ed43
PetarGanchev <lucifer@abv.bg>
2023-01-18 08:57:44 +0200

    update

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,8 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-var products  = document.getElementsByClassName('hot');
-for (let i=0; products.length>i; i++) {
-  let hot = document.getElementsByClassName('hot');
-   hot[i].innerHTML = hot[i].innerHTML+"🔥";
-
-}
-
+  let elementsPrice=document.querySelectorAll('.price'&&'.hot');
+  for(let i=0; i<elementsPrice.length; i++) {
+      elementsPrice[0].innerHTML+=' &#128293;';
+  }
 });
